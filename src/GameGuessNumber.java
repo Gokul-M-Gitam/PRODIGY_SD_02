072570cd5416ed8b75b80ff930beb375fb06bc0c
GokulYashasM <gokulyashas123@gmail.com>
2024-06-15 18:19:21 +0530

    initial commit

--- src/GameGuessNumber.java
+++ src/GameGuessNumber.java
@@ -4,7 +4,7 @@ public class GameGuessNumber
 {
     public static void main(String args[]) throws Exception
     {
-        GameGuess game = new GameGuess();
+        Main game = new Main();
         game.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
         game.setSize(550, 150);
         game.setVisible(true);
